006e7a5cf12ee206710436d098bc7fe1606270ec
unknown <bsa6.23.94@gmail.com>
2018-05-04 12:58:43 -0400

    app working displays projects

---
+++ bob/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+      name: '',
+      description: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getProjects();
+  }
+  getProjects = () => {
+    axios
+      .get(`http://localhost:5000/api/project`)
+      .then(response => {
+        this.setState({ projects: response.data })
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+         {this.state.projects.map(project => {
+         return (
+           <div key={project.id} >
+            <h2> {project.name}</h2>
+            <h4>{project.description}</h4>
+             </div>
+         )
+       })}
+      </div>
+    );
+  }
+}
+
+export default App;
